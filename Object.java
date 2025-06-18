b89657d2ece901a5b066a27984c116486771d8d4
Sanika-Naikwade <202401100010@mitaoe.ac.in>
2025-06-18 14:04:54 +0530

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,14 @@
+public class Object {
+    public void hello(){
+        System.out.println("Hello World");
+    }
+    public void hi(){
+        System.out.println("How are you?");
+    }
+    public static void main(String[] args){
+        Object obj = new Object();
+        obj.hello();
+        obj.hi();
+    }
+    
+}
